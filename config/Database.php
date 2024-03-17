1cb7042693e8405d596d2f461e8f858a4902fe12
kerryomadison <kerryomadison@gmail.com>
2024-03-17 16:52:07 -0700

    trying to fix connection issues in render

--- config/Database.php
+++ config/Database.php
@@ -1,5 +1,5 @@
 <?php
-class Database{
+class Database {
     //DB params
     private $host;
     private $port;
@@ -9,27 +9,27 @@ class Database{
     private $conn;
     
     //DB connect
-public function connect(){
-
-    $this->host= getenv('DATABASE_HOST');
-    $this->port=getenv('DATABASE_PORT'); //5432? 
-    $this->db_name=getenv('DATABASE_NAME');
-    $this->username=getenv('DATABASE_USERNAME');
-    $this->password=getenv('DATABASE_PASSWORD');
+    public function connect(){
+        $this->host = getenv('DATABASE_HOST');
+        $this->port = getenv('DATABASE_PORT'); //5432 (default port for PostgreSQL)
+        $this->db_name = getenv('DATABASE_NAME');
+        $this->username = getenv('DATABASE_USERNAME');
+        $this->password = getenv('DATABASE_PASSWORD');
     
-    $this->conn=null;
-    $dsn = "pgsql:host=" . getenv('DATABASE_HOST') . ";port=" . getenv('DATABASE_PORT') . ";dbname=" . getenv('DATABASE_NAME');
-    $username = getenv('DATABASE_USER');
-    $password = getenv('DATABASE_PASSWORD');
-    try{
-        $this->conn = new PDO($dsn, $username, $password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e){
-        // Throw an exception instead of echoing the error message
-        throw new Exception('Connection Error: '.$e->getMessage());
+        $this->conn = null;
+        $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
+        $username = $this->username;
+        $password = $this->password;
+        try {
+            $this->conn = new PDO($dsn, $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            // Throw an exception instead of echoing the error message
+            throw new Exception('Connection Error: '.$e->getMessage());
+        }
+        return $this->conn;
     }
-    return $this->conn;
 }
-}
+
 ?>
 
