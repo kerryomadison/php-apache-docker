018dd39a52913c0b2785ab3ca758b124650e3f1d
kerryomadison <kerryomadison@gmail.com>
2024-03-17 20:33:54 -0700

    trying to clear up render port issues

--- config/Database.php
+++ config/Database.php
@@ -8,26 +8,29 @@ class Database {
     private $password;
     private $conn;
     
-    //DB connect
-    public function connect(){
+    public function __construct(){
         $this->host = getenv('DATABASE_HOST');
-        //$this->port = getenv('DATABASE_PORT'); //5432 (default port for PostgreSQL)
+        $this->port = getenv('DATABASE_PORT'); //5432 (default port for PostgreSQL)
         $this->db_name = getenv('DATABASE_NAME');
         $this->username = getenv('DATABASE_USERNAME');
         $this->password = getenv('DATABASE_PASSWORD');
-    
-        $this->conn = null;
-        $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
-        $username = $this->username;
-        $password = $this->password;
-        try {
-            $this->conn = new PDO($dsn, $username, $password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            // Throw an exception instead of echoing the error message
-            throw new Exception('Connection Error: '.$e->getMessage());
+    }
+    //DB connect
+    public function connect(){
+        if($this->conn){
+            return $this->conn;
         }
-        return $this->conn;
+        else{
+            $dsn = "pgsql:host={ $this->host};port={$this->port};dbname={$this->db_name};";
+            try {
+                $this->conn = new PDO($dsn, $username, $password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            } catch(PDOException $e) {
+                // Throw an exception instead of echoing the error message
+                throw new Exception('Connection Error: '.$e->getMessage());
+            }
+        }
     }
 }
 
