2164a49c877732138db2fa83cf360bc2e2a6069a
kerryomadison <kerryomadison@gmail.com>
2024-03-17 20:45:41 -0700

    fixing environment variables

--- config/Database.php
+++ config/Database.php
@@ -23,7 +23,7 @@ class Database {
         else{
             $dsn = "pgsql:host={ $this->host};port={$this->port};dbname={$this->db_name};";
             try {
-                $this->conn = new PDO($dsn, $username, $password);
+                $this->conn = new PDO($dsn, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $this->conn;
             } catch(PDOException $e) {
