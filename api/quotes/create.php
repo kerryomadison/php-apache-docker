f958e514b28a506dd9190bc132989570fa37a8d0
kerryomadison <kerryomadison@gmail.com>
2024-03-20 16:50:29 -0700

    update create in quotes folder to return correct messages

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,4 +1,5 @@
 <?php
+
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -15,15 +16,35 @@ $data = json_decode(file_get_contents("php://input"));
 if (empty($data->quote) || strlen($data->quote) > 255 || empty($data->author_id) || empty($data->category_id)) {
     // Return an error response indicating that the input data is invalid
     http_response_code(400); // Bad Request
-    echo json_encode(array("message" => "Missing Required parameters"));
+    echo json_encode(array("message" => "Missing Required Parameters"));
+    exit;
+}
+
+// Check if author_id or category_id is not found
+$database = new Database();
+$pdo = $database->connect();
+
+$stmt = $pdo->prepare("SELECT id FROM authors WHERE id = :author_id");
+$stmt->bindParam(':author_id', $data->author_id);
+$stmt->execute();
+if ($stmt->rowCount() === 0) {
+    // Return 'author_id Not Found' message
+    http_response_code(400); // Bad Request
+    echo json_encode(array('message' => 'author_id Not Found'));
+    exit;
+}
+
+$stmt = $pdo->prepare("SELECT id FROM categories WHERE id = :category_id");
+$stmt->bindParam(':category_id', $data->category_id);
+$stmt->execute();
+if ($stmt->rowCount() === 0) {
+    // Return 'category_id Not Found' message
+    http_response_code(400); // Bad Request
+    echo json_encode(array('message' => 'category_id Not Found'));
     exit;
 }
 
 try {
-    // Create a new instance of the Database class
-    $database = new Database();
-    $pdo = $database->connect();
-
     // Prepare and execute a SQL statement to insert the quote into the database
     $stmt = $pdo->prepare("INSERT INTO quotes (quote, author_id, category_id) VALUES (:quote, :author_id, :category_id)");
     $stmt->bindParam(':quote', $data->quote);
