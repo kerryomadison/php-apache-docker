4375d015be49f56d27e83fb1a80fa402783df5dc
kerryomadison <kerryomadison@gmail.com>
2024-03-20 14:19:44 -0700

    roll back headers just on read single and read for quotes

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,7 +1,4 @@
 <?php
-// Headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
